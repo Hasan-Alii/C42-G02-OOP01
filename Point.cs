8b96047b88383f86d7b865a5be41a66caa39de19
Hasan_Ali <hasan.aly.dev@gmail.com>
2024-07-11 19:57:47 +0300

    Add project files.

---
+++ Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C42_G02_OOP01
+{
+    internal struct  Point
+    {
+        //default access modifiers inside struct is PRIVATE
+        //so we need to change it
+        public int x;
+        public int y;
+
+        //constructor: is a special method
+        //             1. named like struct
+        //             2. has no return type
+        //CLR by default creates a default parameterless constructor
+        //it's used to initialize attributes with the default value
+        //x = 0, y = 0
+        //CLR constructor example
+        //public Point()
+        //{
+        //    x = default;
+        //    y = default;
+        //}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace C42_G02_OOP01
+{
+    //1. class
+    //2. struct
+    //3. interface
+    //4. enum
+
+    //struct Employee
+    //{
+    //    int id;
+    //    string name;
+    //    double salary;
+    //}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            #region demo
+
+            #region Struct ex01
+            //struct: ValueType
+            //stored in the stack
+
+            //Employee
+            //id, namespace, salary
+
+            //point -> x,y
+            Point p1; //declaring object/instance of type type "Point "
+            //CLR allocates uninitialized 8 bytes at stack
+
+            Console.WriteLine(p1.x); //not valid because the x not initialized
+            p1 = new Point();
+            //new here is used to select the constructor
+            #endregion
+
+            #region  
+            #endregion
+
+            #region  
+            #endregion
+
+            #region  
+            #endregion
+
+            #endregion
+        }
+    }
+}
